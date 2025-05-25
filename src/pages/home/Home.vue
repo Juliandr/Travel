b9565e78cfe28dfa48e6f3812c1a2d390c974e0b
pyl <885283317@qq.com>
2025-05-25 16:09:08 +0800

    project init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="home">
+        hello world
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  data () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
