6991ef56fe0ff27e4b27686f14bddd57a4652f81
pyl <885283317@qq.com>
2026-02-05 15:57:48 +0800

    add search to city

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="search">
+      <input type="text" class="search-input" placeholder="搜索城市">
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'CitySearch',
+  data () {
+    return {
+    }
+  },
+  props: {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="stylus">
+  @import '~styles/varibles.styl'
+  .search
+    height: .72rem
+    padding: 0 .1rem
+    background-color: $bgColor
+    .search-input
+      box-sizing: border-box
+      padding: 0 .1rem
+      width: 100%
+      height: .62rem
+      line-height: .62rem
+      text-align: center
+      border-radius: .06rem
+      color: #666
+</style>
