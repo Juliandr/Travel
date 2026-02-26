70f1e9caf7075ef0cbcbdebdfb511e9c9a4fb1cc
pyl <885283317@qq.com>
2026-02-26 09:27:06 +0800

    banner finish

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container" @click="handleGallaryClick">
+    <div class="wrapper">
+      <swiper :options="swiperOption">
+        <swiper-slide v-for="item of imgs" :key="item.index">
+          <img class="gallery-img" :src="item" />
+        </swiper-slide>
+        <div class="swiper-pagination"  slot="pagination"></div>
+      </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CommonGallary',
+  props: {
+    imgs: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  methods: {
+    handleGallaryClick () {
+      this.$emit('close')
+    }
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction',
+        observer: true,
+        observeParents: true
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .container >>> .swiper-container
+    overflow: inherit
+  .container
+    display: flex
+    flex-direction: column
+    justify-content: center
+    position: fixed
+    left: 0
+    right: 0
+    top: 0
+    bottom: 0
+    background: #000
+    z-index: 99
+    .wrapper
+      overflow: hidden
+      height: 0
+      width: 100%
+      padding-bottom: 100%
+      .gallery-img
+        width: 100%
+      .swiper-pagination
+        color: white
+        bottom: -1rem
+</style>
--- src/main.js
+++ src/main.js
@@ -7,13 +7,11 @@ import router from './router'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
-import fastclick from 'fastclick'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import store from './store'
 import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
-fastclick.attach(document.body)
 Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
